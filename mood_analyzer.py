91568aa153113caff7d9e574728347ad3c708bbd
Kaylee Moser <kay.moser@me.com>
2015-10-13 14:09:43 -0700

    search spreadsheet for matching word and print to screen; needs lots of improvement but is base start

--- mood_analyzer.py
+++ mood_analyzer.py
@@ -4,10 +4,23 @@
 # Kaylee Moser
 # Python version 2.7.10
 
+import sys
 from openpyxl import load_workbook
+
+# read in a sentence (tweet)
+tweet = sys.stdin.readline().split()
 
 # read in spreadsheet with values for pos/neg/neut
 wb = load_workbook('test.xlsx')
 ws = wb.active
-print ws['A4'].value
+
+# for each word in tweet, search spreadsheet for matching word
+# NEEDS CHANGING; for now prints matching word
+for word in tweet:
+    for row in ws.iter_rows('A2:A20'):
+        for cell in row:
+            if (word == cell.value):
+                print word
+
+
 
